46cff5b244aa90a0872ef29f074fe5f25f779748
aubreezy0 <aubreezy0@gmail.com>
2018-03-31 17:43:34 -0400

    updates files for auth issues
    major updates to api, events and ui for auth issues. changePassword
    still not working, but these changes are getting it closer.
    also related to https://git.generalassemb.ly/ga-wdi-boston/game-project/issues/1335

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -1,5 +1,5 @@
 'use strict'
-
+// pasted from here on down
 const config = require('../config')
 const store = require('../store')
 
@@ -7,10 +7,8 @@ const signUp = function (data) {
   return $.ajax({
     url: config.apiUrl + '/sign-up',
     method: 'POST',
-    headers: {
-      contentType: 'application/json'
-    },
     data
+    // data: data
   })
 }
 
@@ -18,40 +16,36 @@ const signIn = function (data) {
   return $.ajax({
     url: config.apiUrl + '/sign-in',
     method: 'POST',
-    headers: {
-      contentType: 'application/json'
-    },
     data
+    // data: data
   })
 }
 
-const changePassword = function (data) {
-  return $.ajax({
-    // url: config.apiUrl + '/change-password',
-    url: config.apiOrigin + '/change-password',
-    method: 'PATCH',
-    headers: {
-      contentType: 'application/json',
-      Authorization: 'Token token=' + store.user.token
-    },
-    data
-  })
-}
-
-const signOut = () => {
+const signOut = function () {
   return $.ajax({
     url: config.apiUrl + '/sign-out',
     method: 'DELETE',
     headers: {
-      contentType: 'application/json',
       Authorization: 'Token token=' + store.user.token
     }
+  })
+}
+
+const changePassword = function (data) {
+  console.log('data is ', data)
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
   })
 }
 
 module.exports = {
   signUp,
   signIn,
-  changePassword,
-  signOut
+  signOut,
+  changePassword
 }

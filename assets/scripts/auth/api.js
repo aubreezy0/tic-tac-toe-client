7529144aca860005d3780084f8446691b2388ebf
aubreezy0 <aubreezy0@gmail.com>
2018-03-30 23:45:18 -0400

    update files with project-specific data
    added gameApi and appropriate apiUrls.
    also lots of functions for api.js

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const config = require('../config')
+const store = require('../store')
+
+const signUp = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    headers: {
+      contentType: 'application/json'
+    },
+    data
+  })
+}
+
+const signIn = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    headers: {
+      contentType: 'application/json'
+    },
+    data
+  })
+}
+
+const changePassword = function (data) {
+  return $.ajax({
+    // url: config.apiUrl + '/change-password',
+    url: config.apiOrigin + '/change-password',
+    method: 'PATCH',
+    headers: {
+      contentType: 'application/json',
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const signOut = () => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      contentType: 'application/json',
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut
+}

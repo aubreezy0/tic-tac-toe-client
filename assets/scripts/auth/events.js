83b6aa0a3c39c4b8df749473b242f2cd6f5b6e58
aubreezy0 <aubreezy0@gmail.com>
2018-03-31 17:43:34 -0400

    updates files for auth issues
    major updates to api, events and ui for auth issues. changePassword
    still not working, but these changes are getting it closer.
    also related to https://git.generalassemb.ly/ga-wdi-boston/game-project/issues/1335

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,13 +1,15 @@
 'use strict'
+// pasted from here down...
+const getFormFields = require(`../../../lib/get-form-fields`)
+
 const api = require('./api')
-const getFormFields = require('../../../lib/get-form-fields')
 const ui = require('./ui')
 
 const onSignUp = function (event) {
   event.preventDefault()
-  console.log('event.target is ', event.target)
-  const data = getFormFields(event.target)
-  console.log(data)
+  console.log('sign up ran!')
+
+  const data = getFormFields(this)
   api.signUp(data)
     .then(ui.signUpSuccess)
     .catch(ui.signUpFailure)
@@ -15,28 +17,31 @@ const onSignUp = function (event) {
 
 const onSignIn = function (event) {
   event.preventDefault()
-  const data = getFormFields(event.target)
-  // console.log('in sign in' + data)
-  console.log(data)
+  console.log('sign in ran!')
+
+  const data = getFormFields(this)
   api.signIn(data)
     .then(ui.signInSuccess)
     .catch(ui.signInFailure)
 }
 
-const onChangePassword = function (event) {
-  event.preventDefault()
-  const data = getFormFields(event.target)
-  // console.log('in change password' + data)
-  api.changePassword(data)
-    .then(ui.changePasswordSuccess)
-    .catch(ui.changePasswordFailure)
-}
-
 const onSignOut = function (event) {
   event.preventDefault()
+  console.log('sign out ran')
+
   api.signOut()
     .then(ui.signOutSuccess)
     .catch(ui.signOutFailure)
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+  console.log('change password ran!')
+
+  const data = getFormFields(this)
+  api.changePassword(data)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
 }
 
 const addHandlers = () => {
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,28 +1,64 @@
 'use strict'
 
-const signInSuccess = function () {
+const store = require('../store')
+
+const signUpSuccess = function (data) {
+  $('#message').text('Signed up successfully')
+  $('#message').css('background-color', 'green')
+  console.log('signUpSuccess ran. Data is :', data)
+}
+
+const signUpFailure = function (error) {
+  $('#message').text('Error on sign up')
+  $('#message').css('background-color', 'red')
+  console.error('signUpFailure ran. Error is :', error)
+}
+
+const signInSuccess = function (data) {
   $('#message').text('Successfully signed in')
   $('#message').css('background-color', 'green')
+  console.log('signInSuccess ran. Data is :', data)
+  store.user = data.user
 }
 
-const signInFailure = function () {
+const signInFailure = function (error) {
   $('#message').text('You failed')
   $('#message').css('background-color', 'red')
+  console.error('signInFailure ran. Error is :', error)
 }
 
-const changePasswordSuccess = function () {
+const signOutSuccess = function () {
+  $('#message').text('Signed out successfully')
+  $('#message').css('background-color', 'green')
+  console.log('signOutSuccess ran and nothing was returned!')
+  store.user = null
+}
+
+const signOutFailure = function (error) {
+  $('#message').text('Error on sign out')
+  $('#message').css('background-color', 'red')
+  console.error('signOutFailure ran. Error is :', error)
+}
+
+const changePasswordSuccess = function (data) {
   $('#message').text('Successfully changed password')
   $('#message').css('background-color', 'green')
+  console.log('signUpSuccess ran. Data is :', data)
 }
 
-const changePasswordFailure = function () {
+const changePasswordFailure = function (error) {
   $('#message').text('Password not changed')
   $('#message').css('background-color', 'red')
+  // console.error('changePasswordFailure ran. Error is :', error)
 }
 
 module.exports = {
+  signUpSuccess,
+  signUpFailure,
   signInSuccess,
   signInFailure,
+  signOutSuccess,
+  signOutFailure,
   changePasswordSuccess,
   changePasswordFailure
 }
